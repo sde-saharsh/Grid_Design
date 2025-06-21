9baa5b83b505bbd82ce3b72ece57c27bd2e19c25
saharsh <saharshkhalokar14@gmail.com>
2025-06-21 12:13:54 +0530

    chnages-done

--- frontend/src/components/GridBackgroundDemo.jsx
+++ frontend/src/components/GridBackgroundDemo.jsx
@@ -64,8 +64,8 @@ export function GridBackgroundDemo() {
         className={cn(
           "absolute inset-0",
           "[background-size:80px_80px]",
-          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+          "[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+
         )}
       />
 
